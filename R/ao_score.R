df12007eb8de6393442d0bc54569adca59950671
davidlimbong <davidlimbonggg@gmail.com>
2022-02-22 16:59:10 +0700

    change corgi_score into ao_score

--- R/ao_score.R
+++ R/ao_score.R
@@ -5,7 +5,7 @@ library(dplyr)
 #'
 #' The corgi_score() function is used to assess the Algoritma students based on the quiz on corgi.re.
 #'
-#' @param corgi_url  Link to course on corgi.re (string).
+#' @param path  path to csv file (string).
 #' @param sheet_url Google spreadsheet link (string).
 #' @param sheet_name Sheet name on the spreadsheet (string).
 #' @param column_name The column that you want to update (string).
@@ -16,14 +16,15 @@ library(dplyr)
 #' @export
 #'
 #' @examples
-#' corgi_score(corgi_url = "https://corgi.re/courses/Davidlimbong/P4DS-PS",
+#' ao_score(path = "data_input/QuizReporting_csv/ExploratoryDataAnalysisEDAQuiz-all_0.csv",
 #'       sheet_url = "https://docs.google.com/spreadsheets/d/xxxxx/yyyyy",
 #'       sheet_name = "Academy: Batch 9",
 #'       column_name = "NN Quiz"
 #'       max_score = 4,
 #'       email = "david_at_algorit.ma")
 #'
-corgi_score <- function(corgi_url, sheet_url, sheet_name, column_name, max_score, email) {
+
+ao_score <- function(path, sheet_url, sheet_name, column_name, max_score, email) {
 
   getColumnName <- function(word_length) {
 
@@ -40,13 +41,14 @@ corgi_score <- function(corgi_url, sheet_url, sheet_name, column_name, max_score
     return(col_id)
   }
 
-# Get recipients from corgi
-
-  ## Get participant name
-  corgi <- xml2::read_html(corgi_url) %>%
-    rvest::html_nodes(xpath = "//a") %>%
-    rvest::html_text()
-  corgi <- tolower(corgi[12:(length(corgi)-10)])
+  # Read data from algoritmaonline csv
+  ao <-
+    read.csv(path, stringsAsFactors = T) %>%
+    janitor::clean_names() %>%
+    dplyr::mutate_at(dplyr::vars(passed_status, user_email, display_name), stringr::str_trim, side = "both") %>%
+    dplyr::filter(passed_status == "yes") %>%
+    dplyr::distinct(display_name, user_email) %>%
+    dplyr::pull(user_email)
 
   # Read and write to sheet
   ## Read sheet
@@ -56,12 +58,11 @@ corgi_score <- function(corgi_url, sheet_url, sheet_name, column_name, max_score
 
   academy %>%
     dplyr::filter(!is.na(Class)) %>%
-    dplyr::mutate(`Github ID` = tolower(`Github ID`)) %>%
     transmute_at(.vars = dplyr::vars(dplyr::contains(column_name)),
-                 .funs = function(x) ifelse(.$`Github ID` %in% corgi, max_score,0)) %>% ## Scoring
+                 .funs = function(x) ifelse(.$Email %in% ao, max_score,0)) %>% ## Scoring
     googlesheets4::range_write(ss = sheet_url,
-                sheet = sheet_name,
-                range = getColumnName(id)) # write the result
+                               sheet = sheet_name,
+                               range = getColumnName(id)) # write the result
   googlesheets4::gs4_deauth()
   print("Quiz Updated Successfully")
 
